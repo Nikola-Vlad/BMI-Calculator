50450b7b95c4330ce741b16c54a5dd51f3885624
Nikola <242478580+Nikola-Vlad@users.noreply.github.com>
2026-09-13 19:24:54 +0300

    Added BMI Calculation Functionality with JS for imperial and metric units, bmi result with categories ranging from underweight to obese.

--- script.js
+++ script.js
@@ -1,33 +1,126 @@
-const usUnitsBtn = document.querySelector(".imperial-units-btn");
-const metricUnitsBtn = document.querySelector(".metric-units-btn");
+const imperialUnitsBtn = document.querySelector('.imperial-units-btn');
+const metricUnitsBtn = document.querySelector('.metric-units-btn');
 
-const usHeightLayout = document.querySelector(".imperial-height-flex-container");
-const usWeightLayout = document.querySelector(".imperial-weight-field");
+const imperialHeightLayout = document.querySelector('.imperial-height-flex-container');
+const imperialWeightLayout = document.querySelector('.imperial-weight-field');
 
-const metricHeightLayout = document.querySelector(".metric-height-field");
-const metricWeightLayout = document.querySelector(".metric-weight-field");
+const metricHeightLayout = document.querySelector('.metric-height-field');
+const metricWeightLayout = document.querySelector('.metric-weight-field');
 
-usUnitsBtn.classList.add('active');
+const calculateBtn = document.querySelector('.calculateBtn');
+const bmiResultField = document.querySelector('.bmi-result')
+const resetBtn = document.querySelector('.resetBtn')
 
-usUnitsBtn.addEventListener("click", onUsUnitsClick);
+const feetInput = document.querySelector('.feet-input-bar');
+const inchesInput = document.querySelector('.inches-input-bar');
+const lbsInput = document.querySelector('.lbs-input-bar');
+const cmInput = document.querySelector('.cm-input-bar');
+const kgInput = document.querySelector('.kg-input-bar');
 
-function onUsUnitsClick () {
-    metricHeightLayout.style.display = 'none';
-    metricWeightLayout.style.display = 'none';
-    usHeightLayout.style.display = 'flex';
-    usWeightLayout.style.display = 'block';
-    usUnitsBtn.classList.add('active');
+const bmiValue = document.querySelector('.bmi-value');
+const bmiCategory = document.querySelector('.bmi-category')
+
+
+imperialUnitsBtn.addEventListener("click", onUsUnitsClick);
+
+function onUsUnitsClick() {
+    metricHeightLayout.classList.add('hidden');
+    metricWeightLayout.classList.add('hidden');
+
+    imperialHeightLayout.classList.remove('hidden');
+    imperialWeightLayout.classList.remove('hidden');
+
+    imperialUnitsBtn.classList.add('active');
     metricUnitsBtn.classList.remove('active');
 }
 
 metricUnitsBtn.addEventListener("click", onMetricUnitsClick);
 
-function onMetricUnitsClick () {
-    usHeightLayout.style.display = 'none';
-    usWeightLayout.style.display = 'none';
-    metricHeightLayout.style.display = 'block';
-    metricWeightLayout.style.display = 'block';
+function onMetricUnitsClick() {
+    imperialHeightLayout.classList.add('hidden');
+    imperialWeightLayout.classList.add('hidden');
+
+    metricHeightLayout.classList.remove('hidden');
+    metricWeightLayout.classList.remove('hidden');
+
     metricUnitsBtn.classList.add('active');
-    usUnitsBtn.classList.remove('active');
+    imperialUnitsBtn.classList.remove('active');
 }
 
+calculateBtn.addEventListener('click', onCalculate)
+
+bmiResultField.classList.add('hidden');
+
+let currentCategory;
+
+function onCalculate() {
+    bmiResultField.classList.remove('hidden')
+    let oldCategory = currentCategory;
+    bmiCategory.classList.remove(oldCategory);
+
+    const feet = Number(feetInput.value);
+    const inches = Number(inchesInput.value);
+    const lbs = Number(lbsInput.value);
+    const cm = Number(cmInput.value);
+    const kg = Number(kgInput.value);
+
+    
+    if (imperialUnitsBtn.classList.contains('active')) {
+        const totalInches = feet * 12 + inches;
+        const bmi = (lbs / (totalInches * totalInches)) * 703;
+        bmiValue.textContent = bmi.toFixed(1);
+       
+        if (bmi < 18.5) {
+            bmiCategory.classList.add('category-underweight')
+            bmiCategory.textContent = 'Underweight';
+            currentCategory = 'category-underweight';
+        } else if (bmi < 25) {
+            bmiCategory.classList.add('category-normal')
+            bmiCategory.textContent = 'Normal';
+            currentCategory = 'category-normal';
+        } else if (bmi < 30) {
+            bmiCategory.classList.add('category-overweight')
+            bmiCategory.textContent = 'Overweight';
+            currentCategory = 'category-overweight';
+        } else {
+            bmiCategory.classList.add('category-obese')
+            bmiCategory.textContent = 'Obese';
+            currentCategory = 'category-obese';
+        }
+
+    } else {
+        const heightInMeters = cm / 100;
+        const bmi = kg / (heightInMeters * heightInMeters);
+        bmiValue.textContent = bmi.toFixed(1);
+        
+        if (bmi < 18.5) {
+            bmiCategory.classList.add('category-underweight')
+            bmiCategory.textContent = 'Underweight';
+            currentCategory = 'category-underweight';
+        } else if (bmi < 25) {
+            bmiCategory.classList.add('category-normal')
+            bmiCategory.textContent = 'Normal';
+            currentCategory = 'category-normal';
+        } else if (bmi < 30) {
+            bmiCategory.classList.add('category-overweight')
+            bmiCategory.textContent = 'Overweight';
+            currentCategory = 'category-overweight';
+        } else {
+            bmiCategory.classList.add('category-obese')
+            bmiCategory.textContent = 'Obese';
+            currentCategory = 'category-obese';
+        }
+    }
+
+}
+
+resetBtn.addEventListener('click', onReset)
+
+function onReset() {
+    feetInput.value = '';
+    inchesInput.value = '';
+    lbsInput.value = '';
+    cmInput.value = '';
+    kgInput.value = '';
+    bmiResultField.classList.add('hidden')
+}
