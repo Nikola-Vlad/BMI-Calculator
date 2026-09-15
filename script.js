dd79854246150b881fa99684b4b590172f635365
Nikola <242478580+Nikola-Vlad@users.noreply.github.com>
2026-09-15 15:20:48 +0300

    Add input validation for invalid BMI values

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const metricHeightLayout = document.querySelector('.metric-height-field');
 const metricWeightLayout = document.querySelector('.metric-weight-field');
 
 const calculateBtn = document.querySelector('.calculateBtn');
-const bmiResultField = document.querySelector('.bmi-result')
-const resetBtn = document.querySelector('.resetBtn')
+const bmiResultField = document.querySelector('.bmi-result');
+const resetBtn = document.querySelector('.resetBtn');
 
 const feetInput = document.querySelector('.feet-input-bar');
 const inchesInput = document.querySelector('.inches-input-bar');
@@ -18,8 +18,7 @@ const cmInput = document.querySelector('.cm-input-bar');
 const kgInput = document.querySelector('.kg-input-bar');
 
 const bmiValue = document.querySelector('.bmi-value');
-const bmiCategory = document.querySelector('.bmi-category')
-
+const bmiCategory = document.querySelector('.bmi-category');
 
 imperialUnitsBtn.addEventListener("click", onUsUnitsClick);
 
@@ -47,14 +46,13 @@ function onMetricUnitsClick() {
     imperialUnitsBtn.classList.remove('active');
 }
 
-calculateBtn.addEventListener('click', onCalculate)
+calculateBtn.addEventListener('click', onCalculate);
 
 bmiResultField.classList.add('hidden');
 
 let currentCategory;
 
 function onCalculate() {
-    bmiResultField.classList.remove('hidden')
     let oldCategory = currentCategory;
     bmiCategory.classList.remove(oldCategory);
 
@@ -64,35 +62,49 @@ function onCalculate() {
     const cm = Number(cmInput.value);
     const kg = Number(kgInput.value);
 
-    
     if (imperialUnitsBtn.classList.contains('active')) {
+
+        if (feet <= 0 || feet > 8 || inches < 0 || inches >= 12 || lbs <= 0 || lbs > 500) {
+            alert('Please enter valid values!');
+            return;
+        }
+        bmiResultField.classList.remove('hidden');
+
         const totalInches = feet * 12 + inches;
         const bmi = (lbs / (totalInches * totalInches)) * 703;
         bmiValue.textContent = bmi.toFixed(1);
-       
+
         if (bmi < 18.5) {
-            bmiCategory.classList.add('category-underweight')
+            bmiCategory.classList.add('category-underweight');
             bmiCategory.textContent = 'Underweight';
             currentCategory = 'category-underweight';
         } else if (bmi < 25) {
-            bmiCategory.classList.add('category-normal')
+            bmiCategory.classList.add('category-normal');
             bmiCategory.textContent = 'Normal';
             currentCategory = 'category-normal';
         } else if (bmi < 30) {
-            bmiCategory.classList.add('category-overweight')
+            bmiCategory.classList.add('category-overweight');
             bmiCategory.textContent = 'Overweight';
             currentCategory = 'category-overweight';
         } else {
-            bmiCategory.classList.add('category-obese')
+            bmiCategory.classList.add('category-obese');
             bmiCategory.textContent = 'Obese';
             currentCategory = 'category-obese';
         }
 
     } else {
+
+        if (cm <= 0 || cm > 270 || kg <= 0 || kg > 500) {
+            alert('Please enter valid values!');
+            return;
+        }
+
+        bmiResultField.classList.remove('hidden')
+
         const heightInMeters = cm / 100;
         const bmi = kg / (heightInMeters * heightInMeters);
         bmiValue.textContent = bmi.toFixed(1);
-        
+
         if (bmi < 18.5) {
             bmiCategory.classList.add('category-underweight')
             bmiCategory.textContent = 'Underweight';
